b5e9a4f978cbebb64f0cdb586facdedbac7f4a1f
Aleksandr Hemingway <ghostblac@protonmail.com>
2023-11-27 21:35:59 -0500

    Update main.py
    Updated to remain usable after the sample() function's update.

--- main.py
+++ main.py
@@ -1,53 +1,24 @@
-from random import randint as role1
-from random import sample as role2
-import string
+from random import choice, randint
 
+def role_playing_game(length):
 
-def random_password():
-    """
-    Original version of the random password generator. Note how does not make
-    use of the string package.
-    """
+    password = []
 
-    lower = "abcdefghijklmnopqrstuvwxyz"
-    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    numbers = "123456789"
-    symbols = "[]{}()*;/,._-"
-    characters = lower + upper + numbers + symbols
-    password_length = 24
-    # random.sample was changed to role2 after update
-    password = "".join(role2(characters, password_length))
-    return password
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase = "abcdefghijklmnopqrstuvwxyz"
+    digits = "0123456789"
+    punctuation = "–!”#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
+    character_list = uppercase + lowercase + digits + punctuation
 
-def game():
-    """
-    New version of the random password generator. Makes use of the string
-    package, and obfuscation.
-    """
+    for i in range(length):
 
-    characters = string.ascii_lowercase + string.ascii_uppercase \
-        + string.digits + string.punctuation
-    length = role1(16, 32)
-    playing = role2(characters, length)
-    playing = "".join(playing)
-    return playing
+        random_character = choice(character_list)
+        password.append(random_character)
 
-
-def blind():
-    """
-    Uses the returned values from the previous functions to generate a new
-    value.
-    """
-
-    layer1 = random_password()
-    layer2 = game()
-    layer3 = layer1 + layer2
-    layer4 = (len(layer1) + len(layer2)) // 2
-    layer5 = role2(layer3, layer4)
-    layer6 = "".join(layer5)
-    print(layer6)
-
+    print("".join(password))
 
 if __name__ == "__main__":
-    blind()
+    length = int(input("Enter password length: "))
+    role_playing_game(length)
+    print(randint(1, 25))
