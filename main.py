198d4df24e2fb931123ebfb4eecd9f3a976d1031
Aleksandr Hemingway <ghostblac@protonmail.com>
2024-01-22 16:19:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 from random import choice, randint
 
+
+def create_punctuation():
+
+    punctuation = ""
+
+    setter = input("Simple of complex punctuation?: ")
+
+    if setter == "simple":
+
+        punctuation = "$!#&@?%=_"
+
+        return punctuation
+
+    elif setter == "complex":
+
+        punctuation = "–!”#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+        return punctuation
+
+    else:
+
+        print("Try again.")
+
+
 def role_playing_game(length):
 
     password = []
@@ -7,7 +31,7 @@ def role_playing_game(length):
     uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     lowercase = "abcdefghijklmnopqrstuvwxyz"
     digits = "0123456789"
-    punctuation = "–!”#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    punctuation = str(create_punctuation())
 
     character_list = uppercase + lowercase + digits + punctuation
 
@@ -18,7 +42,37 @@ def role_playing_game(length):
 
     print("".join(password))
 
+
+def role_playing_game_vb():
+    """
+    For websites where the above function generates a password
+    that's considered too complicated.
+    """
+
+    password = []
+
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase = "abcdefghijklmnopqrstuvwxyz"
+    digits = "0123456789"
+
+    character_list = uppercase + lowercase + digits
+
+    for i in range(12):
+
+        random_character = choice(character_list)
+        password.append(random_character)
+
+    print("".join(password))
+
+
 if __name__ == "__main__":
-    length = int(input("Enter password length: "))
-    role_playing_game(length)
+    need = input("Simple or complex?: ")
+
+    if need == "simple":
+        role_playing_game_vb()
+
+    elif need == "complex":
+        length = int(input("Enter password length: "))
+        role_playing_game(length)
+
     print(randint(1, 25))
